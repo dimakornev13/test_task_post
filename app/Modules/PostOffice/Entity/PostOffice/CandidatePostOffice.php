8c0f87b4ce0b2d27c066ce3d4b1fbb28e87fe0cb
vagrant <vagrant@homestead>
2022-05-19 18:32:06 +0000

    - step (almost finished)

--- app/Modules/PostOffice/Entity/PostOffice/CandidatePostOffice.php
+++ app/Modules/PostOffice/Entity/PostOffice/CandidatePostOffice.php
@@ -4,15 +4,24 @@ namespace App\Modules\PostOffice\Entity\PostOffice;
 
 use App\Modules\PostOffice\Entity\Item\ItemAbstract;
 use App\Modules\PostOffice\Entity\Postman\PostmanAbstract;
+use LogicException;
 
 class CandidatePostOffice implements PostOfficeInterface
 {
+    /** @var PostmanAbstract[] */
+    private array $postmen;
+
+    /** @var ItemAbstract[] */
+    private array $postQueue = [];
+
     /**
      * @param PostmanAbstract[] $postmen
      */
     public function __construct(array $postmen)
     {
-        // TODO: Implement __construct() method.
+        $this->postmen = $postmen;
+
+        $this->checkInput('postmen', PostmanAbstract::class);
     }
 
     /**
@@ -22,8 +31,18 @@ class CandidatePostOffice implements PostOfficeInterface
      */
     public function liveDay(array $items = []): array
     {
-        // TODO: Implement liveDay() method.
-        return [];
+        // todo refactor
+        $this->postQueue = array_merge($this->postQueue, $items);
+
+        $this->checkInput('postQueue', ItemAbstract::class);
+
+        $this->unFillPostmen();
+
+        $this->sortPostQueueByExpirationDate();
+
+        $this->fillPostmen();
+
+        return $this->postmen;
     }
 
     /**
@@ -31,8 +50,7 @@ class CandidatePostOffice implements PostOfficeInterface
      */
     public function isEmptyItemsQueue(): bool
     {
-        // TODO: Implement isEmptyItemsQueue() method.
-        return true;
+        return count($this->postQueue) == 0;
     }
 
     /**
@@ -40,7 +58,66 @@ class CandidatePostOffice implements PostOfficeInterface
      */
     public function isAllItemsDelivered(): bool
     {
-        // TODO: Implement isAllItemsDelivered() method.
-        return true;
+        $countPostmenWithPost = collect($this->postmen)
+            ->filter(function (PostmanAbstract $postman) {
+                return $postman->hasItems();
+            })->count();
+
+        return $this->isEmptyItemsQueue() && $countPostmenWithPost === 0;
+    }
+
+    /**
+     * return void
+     * @param string $field
+     * @param string $shouldBeInstanceOf
+     */
+    private function checkInput(string $field, string $shouldBeInstanceOf): void
+    {
+        foreach ($this->{$field} as $postman) {
+            if (($postman instanceof $shouldBeInstanceOf) === false) {
+                throw new LogicException(
+                    sprintf('Given object must be an instance of %s class. %s given.', $shouldBeInstanceOf, get_class($postman))
+                );
+            }
+        }
+    }
+
+    /**
+     * return void
+     */
+    private function fillPostmen(): void
+    {
+        foreach ($this->postmen as $postman) {
+            foreach ($this->postQueue as $key => $post) {
+                if ($postman->isFull())
+                    break;
+
+                if ($postman->getItemFreeSlotCount($post) == 0)
+                    continue;
+
+                $postman->putItem($post);
+                unset($this->postQueue[$key]);
+            }
+        }
+    }
+
+    /**
+     * return void
+     */
+    private function unFillPostmen(): void
+    {
+        foreach ($this->postmen as $postman) {
+            $this->postQueue = array_merge($this->postQueue, $postman->pullAllItems());
+        }
+    }
+
+    /**
+     * return void
+     */
+    private function sortPostQueueByExpirationDate(): void
+    {
+        $this->postQueue = collect($this->postQueue)->sortBy(function (ItemAbstract $post) {
+            return $post->getExpirationDay();
+        })->toArray();
     }
 }
